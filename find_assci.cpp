ac0490459e492128c425ec8987ffea9518b1fbb4
Mohammad Saud <mohammadsaudattari@gmail.com>
2023-08-10 05:52:20 +0500

    find assci code of charceter initial commit

---
+++ find_assci.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <Windows.h>
+using namespace std;
+
+int main(){
+    char ch;
+    cout<<"Enter charcater: ";
+    cin>>ch;
+    Sleep(4000);
+    int n = ch;
+    cout<<"asci code of "<<ch<<" is: "<<n<<endl;
+}
